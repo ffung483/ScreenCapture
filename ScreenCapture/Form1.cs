e11579629d52639e95f8ba48dba1494d3a134fe8
Bùi Quang Phụng <quangphung@outlook.com>
2020-12-06 20:50:14 +0700

    Test change

---
+++ ScreenCapture/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenCapture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Rectangle rect = Screen.PrimaryScreen.Bounds;
+            Bitmap bm = new Bitmap(rect.Width, rect.Height);
+            Graphics g = Graphics.FromImage(bm);
+            g.CopyFromScreen(0, 0, 0, 0, rect.Size);
+            this.BackgroundImage = bm;
+        }
+    }
+}
